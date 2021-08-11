a0378123f4d1ea5cccaff3607eb2da8d0630a9f0
julian <pedrazajulian@hotmail.com>
2021-08-10 23:44:26 -0400

    start

---
+++ controllers/api/projectRoutes.js
@@ -0,0 +1,65 @@
+const router = require('express').Router();
+const { Project } = require('../../models');
+const { sync } = require('../../models/User');
+const withAuth = require('../../utils/auth');
+
+router.post('/', withAuth, async (req, res) => {
+  try {
+    const newProject = await Project.create({
+      ...req.body,
+      user_id: req.session.user_id,
+    });
+
+    res.status(200).json(newProject);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+router.delete('/:id', withAuth, async (req, res) => {
+  try {
+    const projectData = await Project.destroy({
+      where: {
+        id: req.params.id,
+        user_id: req.session.user_id,
+      },
+    });
+
+    if (!projectData) {
+      res.status(404).json({ message: 'No project found with this id!' });
+      return;
+    }
+
+    res.status(200).json(projectData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// Updates project based on its id to mark it completed
+router.put('/:id', withAuth, async (req, res) => {
+  try {
+    const projectData = await Project.update(
+    {
+      is_completed: req.body.is_completed,
+    },  
+    {
+      where: {
+        id: req.params.id,
+        user_id: req.session.user_id,
+      },
+    });
+
+    if (!projectData) {
+      res.status(404).json({ message: 'No project found with this id!' });
+      return;
+    }
+
+    res.status(200).json(projectData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+
+});
+
+module.exports = router;
